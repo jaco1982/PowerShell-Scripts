924c9cd8e4eccd22592a76625eb948bc83986b53
jaco1982 <jaco@hbnet.co.za>
2019-01-25 14:30:57 +0200

    Added to repo

---
+++ DiskUtils/Disk Utils.ps1
@@ -0,0 +1,16 @@
+﻿# (c)2019, Jaco du Plessis
+# Released under GNU GPL
+# This contains a list of quick, custom disk utilities
+
+Function Get-DiskSpace {
+    
+    param (
+        [Parameter(Mandatory=$true)][string]$Name
+    )
+    
+    Get-WmiObject -Class win32_logicalDisk -ComputerName $Name | Select-Object pscomputername, deviceid, @{Name="Free GB"; Expression={[math]::round($_.freespace/1GB, 2)}}, @{Name="Total GB"; Expression={[math]::round($_.size/1GB, 2)}}    
+
+}
+
+
+
---
+++ Windows/Get Disk Space.ps1
@@ -0,0 +1,16 @@
+﻿# (c)2019, Jaco du Plessis
+# Released under GNU GPL
+# This is a quick function to check 
+
+Function Get-DiskSpace {
+    
+    param (
+        [Parameter(Mandatory=$true)][string]$Name
+    )
+    
+    Get-WmiObject -Class win32_logicalDisk -ComputerName $args.Name | Select-Object pscomputername, deviceid, @{Name="Free GB"; Expression={[math]::round($_.freespace/1GB, 2)}}, @{Name="Total GB"; Expression={[math]::round($_.size/1GB, 2)}}    
+
+}
+
+
+
